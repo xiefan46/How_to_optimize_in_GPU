80eaacbdc75a4e02188c4628cf7a817779f9733a
Fan Xie <xiefan.fxie@bytedance.com>
2025-02-21 21:02:19 +0800

    modify

--- reduce/xiefan_reduce_v1.cu
+++ reduce/xiefan_reduce_v1.cu
@@ -19,7 +19,7 @@ __global__ void reduce_kernel_1(float* input, float* output, const int N) {
   __syncthreads();
   #pragma unroll
   for (int i = THREAD_NUM_PER_BLOCK / 2; i > 0; i /= 2) {
-    if (tid + i < 2 * i) {
+    if (tid < i) {
       smem[tid] += smem[tid + i];
     }
     __syncthreads();
