71ffed75c2d390e3ccce6972e1421b22d68114b4
Fan Xie <xiefan.fxie@bytedance.com>
2025-02-22 09:57:57 +0800

    modify

--- reduce/xiefan_reduce_v3.cu
+++ reduce/xiefan_reduce_v3.cu
@@ -8,7 +8,7 @@
 
 const int THREAD_NUM_PER_BLOCK = 256;
 
-const int NUM_PER_THREAD = 8;
+const int NUM_PER_THREAD = 4;
 
 __global__ void reduce_kernel_1(float* input, float* output, const int N) {
   __shared__ float smem[THREAD_NUM_PER_BLOCK];
