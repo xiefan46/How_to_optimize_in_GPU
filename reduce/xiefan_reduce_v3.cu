3475e873de2f502777f61d39b9fa2d584d1b517b
Fan Xie <xiefan.fxie@bytedance.com>
2025-02-22 09:29:18 +0800

    modify

--- reduce/xiefan_reduce_v3.cu
+++ reduce/xiefan_reduce_v3.cu
@@ -17,6 +17,7 @@ __global__ void reduce_kernel_1(float* input, float* output, const int N) {
   const int global_idx = blockDim.x * bid * NUM_PER_THREAD  + tid;
   __shared__ float smem[THREAD_NUM_PER_BLOCK];
   smem[tid] = 0;
+  #pragma unroll
   for (int i = 0; i < NUM_PER_THREAD; i++) {
     const int idx = global_idx + i * bd;
     if (idx < N) {
